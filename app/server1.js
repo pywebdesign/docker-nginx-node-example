217614351143cc5227356a871f92ad2152e81bb7
Pierre-Yves Mathieu <aideaucegep@gmail.com>
2016-11-24 21:00:12 -0500

    add redis

--- app/server1.js
+++ app/server1.js
@@ -1,12 +1,21 @@
 //Lets require/import the HTTP module
 var http = require('http');
+var redis = require('redis');
+var client = redis.createClient({host: "redis"});
 
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
 //Lets define a port we want to listen to
 const PORT=8080;
 
 //We need a function which handles requests and send response
 function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
+    client.incr("string key");
+	client.get("string key", function(err, c){
+		console.log({c: c})
+    	response.end('It Works!! Path Hit: ' + request.url + ' - ' + c);
+	});
 }
 
 //Create a server
